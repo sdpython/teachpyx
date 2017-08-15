ac37efa6499dde48cf1461b2235152440944b8b3
xavier dupré <xavier.dupre@gmail.com>
2017-08-15 10:52:33 +0200

    update notebook unit test

--- _unittests/ut_documentation/test_run_notebooks_python.py
+++ _unittests/ut_documentation/test_run_notebooks_python.py
@@ -40,15 +40,15 @@ except ImportError:
 
 from pyquickhelper.loghelper import fLOG
 from pyquickhelper.pycode import get_temp_folder
-from pyquickhelper.ipythonhelper import execute_notebook_list
+from pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut
 from pyquickhelper.pycode import compare_module_version
 from pyquickhelper.ipythonhelper import install_python_kernel_for_unittest
-import IPython
+import src.teachpyx
 
 
 class TestRunNotebooksPython(unittest.TestCase):
 
-    def test_run_notebook_ml(self):
+    def test_run_notebook_python(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -56,10 +56,6 @@ class TestRunNotebooksPython(unittest.TestCase):
 
         if sys.version_info[0] == 2:
             # notebooks are not converted into python 2.7, so not tested
-            return
-
-        if compare_module_version(IPython.__version__, "4.0.0") < 0:
-            # IPython is not recnt enough
             return
 
         kernel_name = None if "travis" in sys.executable else install_python_kernel_for_unittest(
@@ -90,7 +86,9 @@ class TestRunNotebooksPython(unittest.TestCase):
         addpaths = [os.path.normpath(os.path.join(
             os.path.abspath(os.path.dirname(__file__)), "..", "..", "src")),
             os.path.normpath(os.path.join(
-                os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "pyquickhelper", "src"))
+                os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "pyquickhelper", "src")),
+            os.path.normpath(os.path.join(
+                os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "jyquickhelper", "src"))
         ]
 
         # creation of a kernel
@@ -100,18 +98,8 @@ class TestRunNotebooksPython(unittest.TestCase):
         # run the notebooks
         res = execute_notebook_list(
             temp, keepnote, fLOG=fLOG, valid=valid, additional_path=addpaths, kernel_name=kernel_name)
-
-        # final checkings
-        assert len(res) > 0
-        fails = [(os.path.split(k)[-1], v)
-                 for k, v in sorted(res.items()) if not v[0]]
-        for f in fails:
-            fLOG(f)
-        for k, v in sorted(res.items()):
-            name = os.path.split(k)[-1]
-            fLOG(name, v[0], v[1])
-        if len(fails) > 0:
-            raise fails[0][1][-1]
+        execute_notebook_list_finalize_ut(
+            res, fLOG=fLOG, dump=src.teachpyx)
 
 
 if __name__ == "__main__":
