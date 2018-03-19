ef5873dc8cf327810179fc8bc617825dabec99b5
xavier dupré <xavier.dupre@gmail.com>
2018-03-19 22:33:39 +0100

    move fix for pip

--- _unittests/ut_faq/test_faq_missing.py
+++ _unittests/ut_faq/test_faq_missing.py
@@ -38,13 +38,14 @@ except ImportError:
     import pyquickhelper as skip_
 
 from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import ExtTestCase
 from pyquickhelper.pycode.pip_helper import fix_pip_902
 from src.teachpyx.faq.faq_python import entier_grande_taille, difference_div, python_path, same_variable, stringio
 from src.teachpyx.faq.faq_python import property_example, enumerate_regex_search, sortable_class, list_of_installed_packages
 from src.teachpyx.faq.faq_python import information_about_package, get_month_name, get_day_name
 
 
-class TestFaqMissing (unittest.TestCase):
+class TestFaqMissing(ExtTestCase):
 
     def test_faq_pythonm(self):
         fLOG(
@@ -52,6 +53,7 @@ class TestFaqMissing (unittest.TestCase):
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
+        fix_pip_902()
         entier_grande_taille()
         difference_div()
         python_path()
@@ -59,9 +61,8 @@ class TestFaqMissing (unittest.TestCase):
         same_variable(a, b)
         stringio("e")
         property_example()
-        assert list(enumerate_regex_search("r*", "rararr"))
+        self.assertNotEmpty(list(enumerate_regex_search("r*", "rararr")))
         sortable_class([5, 5])
-        fix_pip_902()
         list_of_installed_packages()
         fLOG(information_about_package("pip"))
         self.assertEqual(get_month_name(datetime(2016, 4, 5)), 'April')
