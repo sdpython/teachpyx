cc3d64adac7ae0b49ab653db93ccb3f30d086822
xavier dupré <xavier.dupre@gmail.com>
2021-01-20 19:59:16 +0100

    change doc theme

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import sphinx_bootstrap_theme
+import pydata_sphinx_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
@@ -9,7 +9,7 @@ local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
 set_sphinx_variables(__file__, "teachpyx", "Xavier Dupré", 2021,
-                     "bootstrap", sphinx_bootstrap_theme.get_html_theme_path(),
+                     "pydata_sphinx_theme", None,
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/teachpyx/issues/%s', 'issue')),
                      title="Programmation avec le langage Python", book=True, nblayout='table')
