53b385f77b8733343f81e1822a838aef481bda2d
xavier dupré <xavier.dupre@gmail.com>
2021-10-09 01:26:28 +0200

    lint

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -20,7 +20,7 @@ class TestCodeStyle(unittest.TestCase):
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'W0212', 'W0107', 'C0415'),
+                                  'W0212', 'W0107', 'C0415', 'C0209'),
                    skip=['construction_classique.py:577: C0200',
                          "Redefining built-in 'format'",
                          "data_bikes.py:1: F0002"])
@@ -36,7 +36,7 @@ class TestCodeStyle(unittest.TestCase):
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0111', 'C0103', 'W0622', 'C1801', 'C0412',
                                   'R0201', 'W0122', 'W0123', 'E1101', 'R1705',
-                                  'W0703', 'W0107', 'C0415'),
+                                  'W0703', 'W0107', 'C0415', 'C0209'),
                    skip=["Unused argument 'cell'",
                          ])
 
