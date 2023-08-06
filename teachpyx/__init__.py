674b7517918b6ef003c4b87a56eebdc95e67d7d9
Xavier Dupre <xadupre@microsoft.com>
2023-08-06 10:49:30 +0200

    upgrading version

--- teachpyx/__init__.py
+++ teachpyx/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-@file
-@brief Module *teachpyx*.
-Materials for a book about programming with :epkg:`Python`.
-"""
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/teachpyx"
 __url__ = "https://sdpython.github.io/doc/teachpyx/dev/"
