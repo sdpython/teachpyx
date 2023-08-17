874f78c1bc0fc11f2a256f5b489a24e4b7256561
Xavier Dupre <xadupre@microsoft.com>
2023-08-17 14:15:44 +0200

    fix ut

--- _unittests/ut_tools/test_data_helper.py
+++ _unittests/ut_tools/test_data_helper.py
@@ -7,7 +7,7 @@ class TestDataHelper(ExtTestCase):
     def test_download_and_unzip(self):
         url = "https://github.com/sdpython/teachpyx/raw/paris/_data/paris_54000.zip"
         data = download_and_unzip(url, ".", verbose=True)
-        self.assertEqual(data[0], "paris_54000.zip")
+        self.assertEqual(data[0], "./paris_54000.txt")
 
 
 if __name__ == "__main__":
