ac12ab9dc10df1b4fa6ef6a04958c2a9a593c94b
xadupre <xadupre@microsoft.com>
2025-02-10 20:26:08 +0100

    issue

--- _doc/examples/prog/plot_pandas_groupby.py
+++ _doc/examples/prog/plot_pandas_groupby.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 """
+.. _l-example-plot-groupby:
+
 =================
 Pandas et groupby
 =================
--- _doc/examples/prog/plot_serialisation_examples.py
+++ _doc/examples/prog/plot_serialisation_examples.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 """
+.. _l-example-serialization:
+
 =============
 Sérialisation
 =============
