df6447adad4224c6848b324944fe054c925127b0
Xavier Dupré <xadupre@microsoft.com>
2026-01-24 19:16:39 +0100

    fix

--- clean.sh
+++ clean.sh
@@ -11,3 +11,8 @@ rm *.pdf
 rm *.dbf
 rm *.xlsx
 rm _unittests/*/temp_* -rf
+rm _doc/c_data/lisezmoi*
+rm _doc/c_data/etatcivil*
+rm _doc/c_data/exemple.txt
+rm _doc/c_data/*.pdf
+rm _doc/c_data/*.dbf
